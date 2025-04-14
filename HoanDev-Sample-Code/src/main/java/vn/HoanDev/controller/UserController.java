3e5b7579182b3f5467376e9c83850fe3a672db60
Trần Đức Hoàn <duchoanlc4@gmail.com>
2025-04-14 16:01:29 +0700

    Response Data

--- HoanDev-Sample-Code/src/main/java/vn/HoanDev/controller/UserController.java
+++ HoanDev-Sample-Code/src/main/java/vn/HoanDev/controller/UserController.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import vn.HoanDev.dto.request.UserRequestDTO;
+import vn.HoanDev.dto.response.ResponseData;
+import vn.HoanDev.dto.response.ResponseFailure;
 import vn.HoanDev.dto.response.ResponseSuccess;
 
 import java.util.List;
@@ -13,43 +15,49 @@ import java.util.List;
 @RequestMapping("user")
 public class UserController {
 
+//    @PostMapping("/")
+//    public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO user) {
+//        return new ResponseData<>(HttpStatus.CREATED.value(), "added user", 1);
+//    }
+//
     @PostMapping("/")
-    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO user) {
-        return new ResponseSuccess(HttpStatus.CREATED, "added user", 1);
+    public ResponseFailure addUser(@Valid @RequestBody UserRequestDTO user){
+        System.out.println("thành công");
+        return new ResponseFailure(HttpStatus.CREATED,"added user");
     }
 
     @PutMapping("/{userId}")
-    public ResponseSuccess updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
+    public ResponseData<?> updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
         System.out.println("update user" + userId);
-        return new ResponseSuccess(HttpStatus.ACCEPTED, "updated user successfully");
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "updated user successfully");
     }
 
     // update một lần thì dùng method patch
     @PatchMapping("/{userId}")
-    public ResponseSuccess changeStatus(@PathVariable int userId, @RequestParam(required = false) boolean status) {
+    public ResponseData<?> changeStatus(@PathVariable int userId, @RequestParam(required = false) boolean status) {
 //        lưu ý: requestParam để required = false là không bắt buộc phải nhập
         System.out.println("request change status, userId=" + userId);
-        return new ResponseSuccess(HttpStatus.ACCEPTED, "changed user successfully");
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "changed user successfully");
     }
 
     @DeleteMapping("{userId}")
-    public ResponseSuccess deleteUser(@PathVariable("userId") int id) {
+    public ResponseData<?> deleteUser(@PathVariable("userId") int id) {
         System.out.println("request delete userId =" + id);
-        return new ResponseSuccess(HttpStatus.NO_CONTENT, "deleted user successfully");
+        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "deleted user successfully");
     }
 
     @GetMapping("{userId}")
-    public ResponseSuccess getUser(@PathVariable int userId) {
+    public ResponseData<UserRequestDTO> getUser(@PathVariable int userId) {
         System.out.println("request get userId=" + userId);
-        return new ResponseSuccess(HttpStatus.OK, "user", new UserRequestDTO("Hoan", "Java", "email", "phone"));
+        return new ResponseData<>(HttpStatus.OK.value(), "user", new UserRequestDTO("Hoan", "Java", "email", "phone"));
     }
 
     @GetMapping("/list")
 
-    public ResponseSuccess getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
-                                      @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
+    public ResponseData<List<UserRequestDTO>> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
+                                                         @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
         System.out.println("Request get all of user");
-        return new ResponseSuccess(HttpStatus.OK, "user all", List.of(new UserRequestDTO("Hoan", "Java", "email", "phone"),
+        return new ResponseData<>(HttpStatus.OK.value(), "user all", List.of(new UserRequestDTO("Hoan", "Java", "email", "phone"),
                 new UserRequestDTO("Ngoc", "Java", "email", "phone")));
     }
 }
---
+++ HoanDev-Sample-Code/src/main/java/vn/HoanDev/dto/response/ResponseFailure.java
@@ -0,0 +1,9 @@
+package vn.HoanDev.dto.response;
+
+import org.springframework.http.HttpStatusCode;
+
+public class ResponseFailure extends ResponseSuccess{
+    public ResponseFailure(HttpStatusCode status, String message) {
+        super(status, message);
+    }
+}
