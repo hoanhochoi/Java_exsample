0187629c87866769bfc3b8727aedf90b4e77f5e2
Trần Đức Hoàn <duchoanlc4@gmail.com>
2025-04-19 16:36:45 +0700

    cors WebMvcConfigurer

---
+++ HoanDev-Sample-Code/src/main/java/vn/HoanDev/configuration/appConfig.java
@@ -0,0 +1,17 @@
+package vn.HoanDev.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class appConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:5173/");
+    }
+}
