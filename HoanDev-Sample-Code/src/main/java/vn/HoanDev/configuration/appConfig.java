e52f53c17688f13c8d516fa4b84cddc860b4e696
Trần Đức Hoàn <duchoanlc4@gmail.com>
2025-04-19 17:07:32 +0700

    CorsFilter cách 2

--- HoanDev-Sample-Code/src/main/java/vn/HoanDev/configuration/appConfig.java
+++ HoanDev-Sample-Code/src/main/java/vn/HoanDev/configuration/appConfig.java
@@ -1,17 +1,30 @@
 package vn.HoanDev.configuration;
 
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
-public class appConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowedOrigins("http://localhost:5173/");
-    }
+public class appConfig  {
+  @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(true);
+      config.setAllowedOrigins(List.of("http://localhost:5173"));
+      config.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+      config.setAllowedHeaders(List.of("*"));
+      source.registerCorsConfiguration("/**",config);
+      FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+      bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+      return bean;
+  }
 }
